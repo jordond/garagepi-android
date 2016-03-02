2a5b9a6be5ba1ce5633dad8b0a8c90e609ddcd50
Jordon de Hoog <jordon.dehoog@gmail.com>
2016-03-02 13:38:01 -0500

    Add more work to the layout

--- app/src/main/java/ca/hoogit/garagepi/Camera/CameraFragment.java
+++ app/src/main/java/ca/hoogit/garagepi/Camera/CameraFragment.java
@@ -27,10 +27,15 @@ package ca.hoogit.garagepi.Camera;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import ca.hoogit.garagepi.R;
 
@@ -39,7 +44,15 @@ import ca.hoogit.garagepi.R;
  */
 public class CameraFragment extends Fragment {
 
-    public CameraFragment() {}
+    @Bind(R.id.container) LinearLayout mContainer;
+    @Bind(R.id.card_camera) CardView mCameraCard;
+    @Bind(R.id.card_weather) CardView mWeatherCard;
+
+    @Bind(R.id.camera_play_stop) ImageButton mPlayStopButton;
+    @Bind(R.id.camera_refresh) ImageButton mRefreshButton;
+
+    public CameraFragment() {
+    }
 
     // TODO: Rename and change types and number of parameters
     public static CameraFragment newInstance() {
@@ -52,7 +65,33 @@ public class CameraFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
         ButterKnife.bind(this, view);
 
+        // Check if landscape and adjust views
+        if (getResources().getBoolean(R.bool.is_landscape)) {
+            mContainer.setOrientation(LinearLayout.HORIZONTAL);
+
+            ViewGroup.LayoutParams cameraParams = mCameraCard.getLayoutParams();
+            cameraParams.width = 0;
+            cameraParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+            mCameraCard.requestLayout();
+
+            ViewGroup.LayoutParams weatherParams = mWeatherCard.getLayoutParams();
+            weatherParams.width = 0;
+            weatherParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+            mWeatherCard.requestLayout();
+        }
+
+        mPlayStopButton.setOnClickListener(this::handlePlayStopButton);
+        mRefreshButton.setOnClickListener(this::handleRefreshButton);
+
         return view;
+    }
+
+    private void handlePlayStopButton(View view) {
+
+    }
+
+    private void handleRefreshButton(View view) {
+
     }
 
 }
